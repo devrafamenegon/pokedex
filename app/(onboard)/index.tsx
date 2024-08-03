36b37655cfc1c7d889a87cc921a64f4ee915ebf2
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-08-03 11:43:10 -0300

    feat: create onboard page

---
+++ app/(onboard)/index.tsx
@@ -0,0 +1,111 @@
+import React, { useRef } from 'react';
+import { Animated, ScrollView, StyleSheet, View, Text, Dimensions, Image, TouchableOpacity } from 'react-native';
+import { CTAButton } from '@/components/CTAButton';
+
+const { width, height } = Dimensions.get('window');
+
+export default function OnboardScreen() {
+  const scrollX = useRef(new Animated.Value(0)).current;
+  const scrollViewRef = useRef<ScrollView>(null);
+
+  const interpolateIndicator1 = scrollX.interpolate({
+    inputRange: [0, width],
+    outputRange: [3 / 4, 1 / 4],
+    extrapolate: 'clamp'
+  });
+
+  const interpolateIndicator2 = scrollX.interpolate({
+    inputRange: [0, width],
+    outputRange: [1 / 4, 3 / 4],
+    extrapolate: 'clamp'
+  });
+
+  const handleContinue = () => {
+    if (scrollViewRef.current) {
+      scrollViewRef.current.scrollTo({ x: width, animated: true });
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <ScrollView
+        ref={scrollViewRef}
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+          { useNativeDriver: false }
+        )}
+        scrollEventThrottle={16}
+      >
+        <View style={styles.page}>
+          <Image source={require('@/assets/images/trainers/duo_1.png')} />
+          <View style={styles.textContainer}>
+            <Text style={styles.title}>Todos os Pokémons em um só Lugar</Text>
+            <Text style={styles.subtitle}>Acesse uma vasta lista de Pokémon de todas as gerações já feitas pela Nintendo</Text>
+          </View>
+          <CTAButton title='Continuar' type='default' onPress={handleContinue} />
+        </View>
+
+        <View style={styles.page}>
+          <Image source={require('@/assets/images/trainers/3.png')} style={styles.image} />
+          <View style={styles.textContainer}>
+            <Text style={styles.title}>Mantenha sua Pokédex atualizada</Text>
+            <Text style={styles.subtitle}>Cadastre-se e mantenha seu perfil, pokémon favoritos, configurações e muito mais, salvos no aplicativo, mesmo sem conexão com a internet.</Text>
+          </View>
+          <CTAButton title='Começar' type='default' />
+        </View>
+      </ScrollView>
+
+      <View style={styles.pageIndicatorContainer}>
+        <Animated.View style={[styles.pageIndicator, { flex: interpolateIndicator1 }]} />
+        <Animated.View style={[styles.pageIndicator, { flex: interpolateIndicator2 }]} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  page: {
+    width: width,
+    height: height,
+    justifyContent: 'flex-end',
+    paddingHorizontal: 19.5,
+    paddingBottom: 80,
+  },
+  image: {
+    alignSelf: 'center',
+  },
+  textContainer: {
+    marginBottom: 40,
+    gap: 10,
+  },
+  title: {
+    textAlign: 'center',
+    fontFamily: 'Poppins-Medium',
+    fontSize: 26,
+  },
+  subtitle: {
+    textAlign: 'center',
+    fontFamily: 'Poppins-Regular',
+    fontSize: 14,
+    lineHeight: 21,
+  },
+  pageIndicatorContainer: {
+    position: 'absolute',
+    bottom: 60,
+    flexDirection: 'row',
+    alignSelf: 'center',
+    width: 45,
+    height: 9,
+  },
+  pageIndicator: {
+    backgroundColor: '#4565B7',
+    borderRadius: 100,
+    marginHorizontal: 4,
+  },
+});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,15 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    "Poppins-Regular": require('../assets/fonts/Poppins-Regular.ttf'),
+    "Poppins-Medium": require('../assets/fonts/Poppins-Medium.ttf'),
+
   });
 
   useEffect(() => {
@@ -27,11 +23,9 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(onboard)" />
+      <Stack.Screen name="login-cadastro" />
+    </Stack>
   );
 }
