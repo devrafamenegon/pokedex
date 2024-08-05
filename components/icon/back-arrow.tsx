4d80f32db048bdcc60aa3c23350980961a93dc3e
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-08-04 23:53:03 -0300

    feat: create back arrow icon component

---
+++ components/icon/back-arrow.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import Svg, { Path, SvgProps } from 'react-native-svg';
+
+const BackArrowIcon = (props: SvgProps) => {
+  return (
+    <Svg
+      width={38}
+      height={38}
+      viewBox="0 0 38 38"
+      fill="none"
+      {...props}
+    >
+      <Path
+        d="M22.166 12.667L15.833 19l6.333 6.333"
+        stroke="#000"
+        strokeWidth={2.5}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </Svg>
+  );
+}
+
+export default BackArrowIcon;
