a91cd97748c9f037ef1e3303cfd15a11fb9b2a55
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-08-06 01:39:16 -0300

    feat: create pokedex layout

---
+++ app/(pokedex)/_layout.tsx
@@ -0,0 +1,9 @@
+import { Stack } from "expo-router";
+
+export default function PokedexLayout() {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="pokedex" />
+    </Stack>
+  );
+}
