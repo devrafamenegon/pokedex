09fcb8c16ccb30fac4525d989e1d5c1aa0f83996
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-08-26 13:18:54 -0300

    feat: add getTextColor function

--- utils/types/colors.ts
+++ utils/types/colors.ts
@@ -1,6 +1,6 @@
 import { PokemonType, PokemonTypes } from "@/types/pokemon";
 
-const typeColors: PokemonTypes = {
+export const typeColors: PokemonTypes = {
   normal: '#919AA2',
   fire: '#FF9D55',
   water: '#5090D6',
@@ -21,6 +21,40 @@ const typeColors: PokemonTypes = {
   fairy: '#EC8FE6',
 };
 
+export const types: string[] = [
+  'normal',
+  'fire',
+  'water',
+  'electric',
+  'grass',
+  'ice',
+  'fighting',
+  'poison',
+  'ground',
+  'flying',
+  'psychic',
+  'bug',
+  'rock',
+  'ghost',
+  'dragon',
+  'dark',
+  'steel',
+  'fairy',
+];
+
 export const getTypeColor = (type: PokemonType): string => {
   return typeColors[type];
+};
+
+export const getTextColor = (backgroundColor: string) => {
+  const color = backgroundColor.substring(1); // Remove o "#"
+  const rgb = parseInt(color, 16); // Converte para inteiro RGB
+  const r = (rgb >> 16) & 0xff; // Extrai o componente vermelho
+  const g = (rgb >> 8) & 0xff; // Extrai o componente verde
+  const b = (rgb >> 0) & 0xff; // Extrai o componente azul
+
+  const brightness = (r * 299 + g * 587 + b * 114) / 1000; // Calcula o brilho
+
+  // Se o brilho for alto, use preto; caso contrário, use branco
+  return brightness > 146 ? '#000' : '#fff';
 };
