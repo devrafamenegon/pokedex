6d0709df866148afbfeb177c1e46c86d19ad7feb
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-09-14 07:16:27 -0300

    feat: create format pokemon number util function

--- utils/string.ts
+++ utils/string.ts
@@ -1,6 +1,12 @@
 export const capitalizeFirstLetter = (text: string) => {
+  if (!text[0]) return;
+  
   const firstLetter = text[0].toUpperCase();
   const restOfText = text.slice(1);
 
   return firstLetter + restOfText;
+}
+
+export const formatPokemonNumber = (id: number) => {
+  return (id/1000).toFixed(3).split('.').join("")
 }
