4dd21c58b3c58a79c7611bfb9038833111a0c9dc
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-09-16 20:05:11 -0300

    feat: add isBorderless parameter into pokemon type icon component

--- components/PokemonTypeIcon.tsx
+++ components/PokemonTypeIcon.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React from "react";
+import { View, StyleSheet } from "react-native";
 import { PokemonType } from "@/types/pokemon";
 import { getTypeColor } from "@/utils/types/colors";
-import { getTypeFlatIcon } from '@/utils/types/flat';
+import { getTypeFlatIcon } from "@/utils/types/flat";
 
-const PokemonTypeIcon: React.FC<{ type: PokemonType }> = ({ type }) => {
+const PokemonTypeIcon: React.FC<{
+  type: PokemonType;
+  isBorderless?: boolean;
+}> = ({ type, isBorderless = false }) => {
   const color = getTypeColor(type);
   const Icon = getTypeFlatIcon(type);
-  
+
   return (
-    <View style={styles.iconContainer}>
-      <Icon fill={color} width={13} height={13}/>
+    <View
+      style={[
+        styles.iconContainer,
+        { backgroundColor: isBorderless ? "transparent" : "#fff" },
+      ]}
+    >
+      <Icon fill={isBorderless ? "#fff" : color} width={13} height={13} />
     </View>
   );
 };
@@ -20,9 +28,8 @@ const styles = StyleSheet.create({
     width: 20,
     height: 20,
     borderRadius: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff'
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
 
