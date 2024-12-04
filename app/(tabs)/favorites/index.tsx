b73c6e74aaba8cd369114a259ed6a427d8b7abd1
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-12-04 19:39:22 -0300

    feat: create favorites screen

--- app/(tabs)/favorites/index.tsx
+++ app/(tabs)/favorites/index.tsx
@@ -1,12 +1,38 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
+import PokemonList from "../pokedex/components/PokemonList";
+import { useFavorites } from "@/contexts/favorites";
+import { Pokemon } from "@/types/pokemon";
+import { useEffect, useState } from "react";
+import MagikarpIcon from "@/components/icon/magikarp";
 
 export default function FavoritesScreen() {
+  const { favorites, isLoading } = useFavorites();
+
+  const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    if (!isLoading) setPokemonList(favorites);
+  }, [favorites, isLoading]);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Favoritos</Text>
-      <Text style={styles.text}>
-        Aqui estão os Pokémon que você marcou como favoritos!
-      </Text>
+      {!isLoading && pokemonList.length ? (
+        <View style={styles.listContainer}>
+          <PokemonList isLoading={isLoading} data={pokemonList} />
+        </View>
+      ) : (
+        <View style={styles.emptyListContainer}>
+          <MagikarpIcon />
+          <Text style={styles.strongText}>
+            Você não favoritou nenhum Pokémon :(
+          </Text>
+          <Text style={styles.descriptionText}>
+            Clique no ícone de coração dos seus pokémons favoritos e eles
+            aparecerão aqui.
+          </Text>
+        </View>
+      )}
     </View>
   );
 }
@@ -14,19 +40,35 @@ export default function FavoritesScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#f5f5f5",
+    backgroundColor: "#fff",
+    paddingHorizontal: 16,
   },
   title: {
     fontSize: 24,
     fontWeight: "bold",
-    marginBottom: 8,
+    marginVertical: 20,
   },
-  text: {
-    fontSize: 16,
-    color: "#555",
+  listContainer: {
+    marginTop: 16,
+    flex: 1,
+  },
+  emptyListContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  strongText: {
+    marginTop: 16,
+    color: "#333",
     textAlign: "center",
-    marginHorizontal: 16,
+    fontFamily: "Poppins-SemiBold",
+    fontSize: 20,
+  },
+  descriptionText: {
+    marginTop: 8,
+    color: "#333",
+    textAlign: "center",
+    fontFamily: "Poppins-Regular",
+    fontSize: 14,
   },
 });
